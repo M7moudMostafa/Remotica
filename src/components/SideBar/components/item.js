6d2d3d0ae4dee57113e2b80e45ebc569530345f0
Mahmoud Mostafa <156802364+M7moudMostafa@users.noreply.github.com>
2025-08-13 16:46:16 +0300

    Add selected styles (#9)

--- src/components/SideBar/components/item.js
+++ src/components/SideBar/components/item.js
@@ -2,23 +2,31 @@ import {
   setFocus,
   useFocusable,
 } from "@noriginmedia/norigin-spatial-navigation";
+import { useAtomValue } from "jotai";
 import { useEffect } from "react";
 import styled, { css } from "styled-components";
+import { activeComponentAtom } from "../../../stores/JotaiStore";
 
 const Item = ({ item, onEnterPress }) => {
-  const { ref, focused } = useFocusable({
+  const { ref, focused, focusKey } = useFocusable({
     focusKey: item.focusKey,
     onEnterPress: () => {
       onEnterPress(item.title);
     },
   });
 
+  const activeComponent = useAtomValue(activeComponentAtom);
+
   useEffect(() => {
     setFocus("Main");
   }, []);
 
   return (
-    <MenuItem ref={ref} $focused={focused}>
+    <MenuItem
+      ref={ref}
+      $focused={focused}
+      $selected={activeComponent === focusKey}
+    >
       {item.icon} {item.title}
     </MenuItem>
   );
@@ -26,6 +34,18 @@ const Item = ({ item, onEnterPress }) => {
 
 const MenuItem = styled.div`
   transition: transform 0.3s ease, background-color 0.3s ease;
+
+  ${({ $selected }) =>
+    $selected &&
+    css`
+      font-size: 1.5rem;
+      background: linear-gradient(to right, #7cbfff, #3d00b6);
+      padding: 1rem;
+      border-radius: 1rem;
+      color: #011a32;
+      transform: scale(1.05);
+    `}
+
   ${({ $focused }) =>
     $focused &&
     css`
