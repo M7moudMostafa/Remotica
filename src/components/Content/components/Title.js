3e62ebedf34abf59a0f9dfcb535b9e4550eaa24f
Mahmoud Mostafa <156802364+M7moudMostafa@users.noreply.github.com>
2025-08-13 15:34:07 +0300

    Change font weight (#2)

--- src/components/Content/components/Title.js
+++ src/components/Content/components/Title.js
@@ -11,7 +11,7 @@ const CustomTitle = styled(Title)`
   margin: 0;
   padding: 0;
   font-size: 1.5rem !important;
-  font-weight: 600 !important;
+  font-weight: 500 !important;
 `;
 
 export default TitleComponent;
