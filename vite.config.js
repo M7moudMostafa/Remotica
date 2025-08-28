74725dd4e3e4a5c055c80d21c102b92f67216d40
Mahmoud Mostafa <156802364+M7moudMostafa@users.noreply.github.com>
2025-08-28 20:41:28 +0300

    Add Vite Configs

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      png: {
+        quality: 50,
+      },
+    }),
+  ],
   esbuild: {
-    loader: 'jsx',
+    loader: "jsx",
     include: /src\/.*\.js$/,
     exclude: [],
-  }
-})
+  },
+  server: {
+    port: 3000,
+    host: true,
+    open: true,
+    strictPort: true,
+  },
+  build: {
+    outDir: "build",
+    sourcemap: true,
+    assetsDir: "assets",
+    target: "es2015",
+    chunkSizeWarningLimit: 500,
+  },
+});
